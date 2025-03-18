f94cd22399bc2aadccb651e2c53682fc69c88ec7
MelisaBunjaku19 <120101000+MelisaBunjaku19@users.noreply.github.com>
2025-03-18 11:09:27 +0100

    Fixed index.js

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
+  res.send(`
+    <html>
+      <head>
+        <title>Welcome to My Awesome App!</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            background: linear-gradient(to right, #ff7e5f, #feb47b);
+            color: white;
+            padding: 50px;
+          }
+          h1 {
+            font-size: 3em;
+            margin-bottom: 20px;
+          }
+          p {
+            font-size: 1.5em;
+            margin-top: 10px;
+          }
+          .emoji {
+            font-size: 2em;
+          }
+        </style>
+      </head>
+     </body>
+    </html>
+  `);
+});
 const PORT = 8080
 
 app.listen(PORT, () => {
